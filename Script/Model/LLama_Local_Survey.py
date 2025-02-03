f124e94142ccd54aa9c654ba255038e55c42d2ad
Marco Cuccarini <59500902+marcocuccarini@users.noreply.github.com>
2025-02-03 09:36:57 +0100

    Update

---
+++ Script/Model/LLama_Local_Survey.py
@@ -0,0 +1,32 @@
+class Local_LLama_Survey():
+
+  def __init__(self):
+    
+    import ollama
+
+    self.client = ollama.Client(host="http://127.0.0.1:11434")
+
+
+
+
+
+  def answer(self, question, real_answer, answer, text):
+
+    
+    #pos
+
+    response = self.client.chat (model='llama3.2:3b', messages=[
+      {'role': 'system', 'content': "Sei l'assistente ad un sondaggio in lingua italiana, ad una persona saranno poste delle domande rigurdanti un testo. Tu dovrai controllare se sono giuste o no, e motivare brevemente il perchè secondo quel testo",
+       'role': 'user', 'content': "La domanda posta dal sondaggio era la segurente:'"+ question+"', la risposta dell'utente è stata:'"+answer+"'e il testo su cui erano posto le damande era:'"+text+"'. Considerando che la risposta corretta è:'"+real_answer+"'', e motivarmi brevemente il perchè?"}])
+
+
+    if 'message' in response and 'content' in response['message']: 
+      return response['message']['content'].strip()
+
+
+    else:
+
+
+      return "Non posso rispondere alla domanda."
+
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests,os
 from dotenv.main import load_dotenv
 from langchain.memory import ConversationSummaryBufferMemory
 import torch
-
+from data import dataset_QA_survey
 
 from Script.Structure.Document import Document
 from Script.Model.BERTModel import BERT_Model
@@ -12,6 +12,8 @@ from Script.Pipeline.PipelineChat import Pipeline_Chatbot
 from Script.Varius import util
 from sentence_transformers import SentenceTransformer
 from Script.Model.LLama_Local import Local_LLama
+from Script.Model.LLama_Local_Survey import Local_LLama_Survey
+
 #from Script.Model.LLama_API import Local_LLama
 
 
@@ -21,8 +23,8 @@ from Script.Model.LLama_Local import Local_LLama
 model_doc=Document('Dataset/','Document.csv')
 model_BERT=BERT_Model("paraphrase-multilingual-mpnet-base-v2", SentenceTransformer )
 model_doc.load_document(0)
-
 model_LLama=Local_LLama()
+model_LLama_Survey=Local_LLama_Survey()
 
 #memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=100)
 app = Flask(__name__)
@@ -37,8 +39,6 @@ def get_data():
 
     data = request.get_json()
 
-    print(data)
-
     if data['mode']=="domande":
 
         return question(data)
@@ -50,24 +50,21 @@ def get_data():
 
 def survey(data):
 
+    answer=data.get('data')
+    index=data.get('cont')
+    try:
+        chatbot1=Pipeline_Chatbot(model_BERT,dataset_QA_survey[index][0],model_doc.document_splitting('£'),model_doc.document_encoding(model_BERT),0)
+        output1 = chatbot1.pred_best_answer().replace("\n","")
+        output=model_LLama_Survey.answer(dataset_QA_survey[index][0], dataset_QA_survey[index][1], answer, output1)
 
-    print(data)
+        #memory.save_context({"input": user_input}, {"output": output})
+        return jsonify({"response":True,"message":output,"nextquestion": dataset_QA_survey[index+1][0]})
 
+    except Exception as e:
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        print(e)
+        error_message = f'Error: {str(e)}'
+        return jsonify({"message":error_message,"response":False})
 
 def question(data):
 
@@ -78,12 +75,6 @@ def question(data):
         chatbot=Pipeline_Chatbot(model_BERT,question,model_doc.document_splitting('£'),model_doc.document_encoding(model_BERT),0)
         output = chatbot.pred_best_answer().replace("\n","")
         output=model_LLama.answer(question, output)
-
-
-        print(output)
-
-
-
 
         #memory.save_context({"input": user_input}, {"output": output})
         return jsonify({"response":True,"message":output})
