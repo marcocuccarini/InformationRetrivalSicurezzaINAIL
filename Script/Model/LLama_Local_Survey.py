8a99a610402c54273e89e65910fdb00371c18b67
Marco Cuccarini <59500902+marcocuccarini@users.noreply.github.com>
2025-02-03 19:31:57 +0100

    update

--- Script/Model/LLama_Local_Survey.py
+++ Script/Model/LLama_Local_Survey.py
@@ -17,7 +17,7 @@ class Local_LLama_Survey():
 
     response = self.client.chat (model='llama3.2:3b', messages=[
       {'role': 'system', 'content': "Sei l'assistente ad un sondaggio in lingua italiana, ad una persona saranno poste delle domande rigurdanti un testo. Tu dovrai controllare se sono giuste o no, e motivare brevemente il perchè secondo quel testo",
-       'role': 'user', 'content': "La domanda posta dal sondaggio era la segurente:'"+ question+"', la risposta dell'utente è stata:'"+answer+"'e il testo su cui erano posto le damande era:'"+text+"'. Considerando che la risposta corretta è:'"+real_answer+"'', e motivarmi brevemente il perchè?"}])
+       'role': 'user', 'content': "La domanda posta dal sondaggio era la segurente:'"+ question+"', la risposta dell'utente è stata:'"+answer+"'e il testo su cui erano posto le damande era:'"+text+"'. Considerando che la risposta corretta è:'"+real_answer+"'', e motivarmi molto brevemente il perchè. Ricorda che la persona in question è un videoterminalista che deve seguire le istruzioni presenti nel documento. "}])
 
 
     if 'message' in response and 'content' in response['message']: 
